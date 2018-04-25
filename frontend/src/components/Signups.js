1783191c91f95be87c37f013300aebbef2b22705
Fprince333 <lwilsonsmith@gmail.com>
2018-04-25 13:02:26 -0400

    Add missing data attribute

--- frontend/src/components/Signups.js
+++ frontend/src/components/Signups.js
@@ -2,11 +2,11 @@ import React from 'react';
 import axios from "axios";
 import Skeleton from "react-loading-skeleton";
 
-class Signups extends Component {
+class Signups extends React.Component {
   state = {}
   componentDidMount() {
     axios.get('/api/signups').then(res => {
-      const signups = res;
+      const signups = res.data;
       this.setState({signups: signups})
     })
   }
