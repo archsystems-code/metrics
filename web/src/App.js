71525f38dfdb0642c0a2f68ef197333bdb048703
Fprince333 <lwilsonsmith@gmail.com>
2018-05-02 12:27:13 -0400

    Add revenue component

--- web/src/App.js
+++ web/src/App.js
@@ -3,6 +3,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 import Signups from './components/Signups';
 import SampleRequests from './components/SampleRequests';
+import Revenue from './components/Revenue';
 import logo from './logo.svg';
 import './App.css';
 
@@ -24,6 +25,7 @@ class App extends Component {
           <div className="App-container">
             <Signups date={this.state.date}/>
             <SampleRequests date={this.state.date}/>
+            <Revenue date={this.state.date} />
           </div>
         </React.Fragment>
       </MuiThemeProvider>
---
+++ web/src/components/Revenue.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from "axios";
+import Paper from 'material-ui/Paper';
+
+class Revenue extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      date: props.date
+    }
+  }
+  componentDidMount() {
+    axios.get('https://asi-metrics.herokuapp.com/api/sales').then(res => {
+      const sales = res.data;
+      this.setState({sales: sales})
+    })
+  }
+  render() {
+    const paperStyle = {
+      height: 125,
+      width: 125,
+      margin: 20,
+      padding: 10,
+      textAlign: 'center',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      backgroundColor: '#DD4E75'
+    };
+    let totalSales = "0.00"
+    if (this.state.sales) {
+      totalSales = this.state.sales.filter(request => new Date(request.created_at).getMonth() === this.state.date.getMonth()).map(sale => Number(sale.total.replace(/[^0-9\\.-]+/g,""))).reduce((acc, val) => acc + val)
+    }
+    return <Paper style={paperStyle} zDepth={2}> <div><p>Total Revenue</p><p>${totalSales}</p></div></Paper>
+  }
+}
+
+export default Revenue;
